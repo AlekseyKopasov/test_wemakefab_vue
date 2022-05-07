f5e0ba9ee98ae51b7deb58c9cc2d557e149d1177
Aleksey Kopasov <alesey.k1@gmail.com>
2022-05-07 23:11:35 +0300

    WIP: добавляет компонент с фильтрами

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
+    <p>search</p>
+    <p>Product category select</p>
+    <p>Sale select</p>
+    <p>Apply Button submit</p>
+
     <b-form inline>
-      <label class="mr-sm-2" for="input-group-1">Name</label>
+      <label class="mr-sm-2" for="input-search">Search</label>
+      <b-form-input aria-label="input-search"></b-form-input>
+
+      <label class="mr-sm-2" for="input-category">Product category</label>
       <b-form-select v-model="selected" :options="options"></b-form-select>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
+      <label class="mr-sm-2" for="input-sale">Product category</label>
+      <b-form-select v-model="selected" :options="options"></b-form-select>
+
+      <b-button type="submit" variant="primary">Apply</b-button>
     </b-form>
   </div>
 </template>
@@ -20,8 +31,8 @@ export default {
         { value: 'b', text: 'Selected Option' },
         { value: { C: '3PO' }, text: 'This is an option with object value' },
         { value: 'd', text: 'This one is disabled', disabled: true }
-      ]
-    }
-  }
-}
+      ],
+    };
+  },
+};
 </script>

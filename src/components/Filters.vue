2344e67dab5c568af88ce944eab5ad74b4eacbf7
Aleksey Kopasov <alesey.k1@gmail.com>
2022-05-13 01:16:58 +0300

    WIP: добавляет компонент с фильтрами

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,21 +1,24 @@
 <template>
   <div>
-    <p>search</p>
-    <p>Product category select</p>
-    <p>Sale select</p>
-    <p>Apply Button submit</p>
+    <b-form class="filters-form" inline>
+      <b-form-row>
+        <b-input-group class="d-flex flex-column align-items-start col">
+          <label for="input-search">Search</label>
+          <b-form-input aria-label="input-search"></b-form-input>
+        </b-input-group>
 
-    <b-form inline>
-      <label class="mr-sm-2" for="input-search">Search</label>
-      <b-form-input aria-label="input-search"></b-form-input>
+        <b-input-group class="d-flex flex-column align-items-start col">
+          <label for="input-category">Product category</label>
+          <b-form-select v-model="selected" :options="options"></b-form-select>
+        </b-input-group>
 
-      <label class="mr-sm-2" for="input-category">Product category</label>
-      <b-form-select v-model="selected" :options="options"></b-form-select>
+        <b-input-group class="d-flex flex-column align-items-start col">
+          <label for="input-sale">Sale</label>
+          <b-form-select v-model="selected" :options="options"></b-form-select>
+        </b-input-group>
 
-      <label class="mr-sm-2" for="input-sale">Product category</label>
-      <b-form-select v-model="selected" :options="options"></b-form-select>
-
-      <b-button type="submit" variant="primary">Apply</b-button>
+        <b-button class="col" type="submit" variant="primary">Apply</b-button>
+    </b-form-row>
     </b-form>
   </div>
 </template>
@@ -36,3 +39,54 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/style.scss";
+
+.filters-form {
+  .form-row {
+    align-items: flex-end;
+  }
+
+  .input-group,
+  .custom-select {
+    font-family: inherit;
+    font-size: 18px;
+    line-height: 22px;
+    letter-spacing: 0;
+    font-weight: 500;
+    text-align: left;
+  }
+
+  label {
+    padding-bottom: 20px;
+  }
+
+  .custom-select,
+  .form-control {
+    width: 100%;
+    height: auto;
+    padding: 20px;
+    padding-right: 26px;
+    box-sizing: border-box;
+    border: none;
+    border-radius: 0;
+    background: $color-white-smoke;
+    color: $color-black;
+    cursor: pointer;
+  }
+
+  button[type="submit"] {
+    padding: 20px;
+    border: none;
+    border-radius: 0;
+
+    font-size: 18px;
+    line-height: 22px;
+    letter-spacing: 0;
+    font-weight: 500;
+    background-color: $color-nero;
+    color: $color-white;
+  }
+}
+</style>

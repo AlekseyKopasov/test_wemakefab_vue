6869a9d5ee1e2ca61321692ef068945792590f5d
Aleksey Kopasov <alesey.k1@gmail.com>
2022-05-23 13:38:57 +0300

    WIP: стилизует компонент с фильтрами

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -3,21 +3,23 @@
     <b-form class="filters-form" inline>
       <b-form-row>
         <b-input-group class="d-flex flex-column align-items-start col">
-          <label for="input-search">Search</label>
+          <label>Search</label>
           <b-form-input aria-label="input-search"></b-form-input>
         </b-input-group>
 
         <b-input-group class="d-flex flex-column align-items-start col">
-          <label for="input-category">Product category</label>
+          <label>Product category</label>
           <b-form-select v-model="selected" :options="options"></b-form-select>
         </b-input-group>
 
         <b-input-group class="d-flex flex-column align-items-start col">
-          <label for="input-sale">Sale</label>
+          <label>Sale</label>
           <b-form-select v-model="selected" :options="options"></b-form-select>
         </b-input-group>
 
-        <b-button class="col" type="submit" variant="primary">Apply</b-button>
+        <b-input-group class="d-flex flex-column align-items-start col">
+          <b-button class="col" type="submit" variant="primary">Apply</b-button>
+        </b-input-group>
     </b-form-row>
     </b-form>
   </div>
@@ -33,7 +35,7 @@ export default {
         { value: 'a', text: 'This is First option' },
         { value: 'b', text: 'Selected Option' },
         { value: { C: '3PO' }, text: 'This is an option with object value' },
-        { value: 'd', text: 'This one is disabled', disabled: true }
+        { value: 'd', text: 'This one is disabled', disabled: true },
       ],
     };
   },
@@ -42,6 +44,13 @@ export default {
 
 <style lang="scss" scoped>
 @import "../assets/styles/style.scss";
+
+select {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  text-indent: 1px;
+  text-overflow: '';
+}
 
 .filters-form {
   .form-row {
@@ -71,9 +80,22 @@ export default {
     box-sizing: border-box;
     border: none;
     border-radius: 0;
-    background: $color-white-smoke;
+    background-color: $color-white-smoke;
     color: $color-black;
     cursor: pointer;
+  }
+
+  .custom-select {
+    //background:
+    //  $color-white-smoke
+    //  url("../assets/images/icons/icon-arrow-down.svg")
+    //  no-repeat;
+    //background-size: 14px 7px;
+    //background-position: 94% 50%;
+
+    &:focus {
+      transform: rotate(180deg);
+    }
   }
 
   button[type="submit"] {
